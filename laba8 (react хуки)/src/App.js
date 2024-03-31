e2b82424dd403656a5c930134e97c745b11f8489
alaxaboom <125747499+alaxaboom@users.noreply.github.com>
2024-03-31 14:41:45 +0700

    8 laba

--- laba8 (react хуки)/src/App.js
+++ laba8 (react хуки)/src/App.js
@@ -1,10 +1,22 @@
+import React from 'react';
+import Timer from './task1';
+import InfiniteTimer from './task2';
+import PrimeNumbers from './task3';
+import Svetofor from './task4';
+import Revert from './task5';
 
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-
+    <div className='flex-container'>
+      <Timer className='flex-item'/>
+      <div className='divider'></div>
+      <InfiniteTimer className='flex-item'/>
+      <div className='divider'></div>
+      <PrimeNumbers className='flex-item'/>
+      <div className='divider'></div>
+      <Svetofor className='flex-item'/>
+      <div className='divider'></div>
+      <Revert s="abcdefg" className='flex-item'/>
     </div>
   );
 }
