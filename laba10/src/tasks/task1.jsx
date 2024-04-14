f5b0ad2238f7b22425d322339484cfed8738aae0
alaxaboom <125747499+alaxaboom@users.noreply.github.com>
2024-04-14 23:25:43 +0700

    /

--- laba10/src/tasks/task1.jsx
+++ laba10/src/tasks/task1.jsx
@@ -3,20 +3,21 @@ import React, { useState } from 'react';
 const FormComponent = () => {
   const [postData, setPostData] = useState(null);
   const [userData, setUserData] = useState(null);
-
-  const fetchData = async (id) => {
+  const [id, setId]= useState();
+    const fetchData = async (id) => {
     const postResponse = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
     const postData = await postResponse.json();
     setPostData(postData);
 
-    const userResponse = await fetch(`https://jsonplaceholder.typicode.com/users/${postData.userId}`);
+   const userResponse = await fetch(`https://jsonplaceholder.typicode.com/users/${postData.userId}`);
     const userData = await userResponse.json();
-    setUserData(userData);
+    setUserData(userData); 
   };
-
+ 
   return (
     <div>
-      <button onClick={() => fetchData(1)}>Загрузить данные</button>
+     <input type="text" value={id} onChange={(e) => setId(e.target.value)}/>
+      <button onClick={() => fetchData(id)}>Загрузить данные</button>
       {postData && userData && (
         <div>
           <h2>{postData.title}</h2>
@@ -24,7 +25,7 @@ const FormComponent = () => {
           <p><strong>Имя пользователя:</strong> {userData.name}</p>
           <p><strong>Email:</strong> {userData.email}</p>
         </div>
-      )}
+      )} 
     </div>
     
   );
