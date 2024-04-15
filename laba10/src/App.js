417e29891082faa864f77277fd403a663b86fd7d
Опивалов Егор Константинович <OpivalovEK@DOM201.LOCAL>
2024-04-15 10:34:31 +0700

    aaa

--- laba10/src/App.js
+++ laba10/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import FormComponent from "./tasks/task1";
+import RegistrationForm from './tasks/task2';
 function App() {
   return (
     <div className='flex-container'>
       <div className="flex-item"><FormComponent /></div>
-      <div className="flex-item"></div>
+      <div className="flex-item"><RegistrationForm/></div>
     </div>
   );
 }
