9bf692a5eff0141ce4c866803ae28355f7a90527
alaxaboom <125747499+alaxaboom@users.noreply.github.com>
2024-05-05 16:39:57 +0700

    Delete laba4(node и npm чуть чуть) directory

--- laba4(node и npm чуть чуть)/part1/task1.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-function reverseFile(filename) {
-  const content = fs.readFileSync(filename, 'utf-8');
-  const reversedContent = content.split('').reverse().join('');
-  fs.writeFileSync(filename, reversedContent);
-}
-
-reverseFile('text.txt');
