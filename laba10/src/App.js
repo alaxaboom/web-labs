e8847301ce275ff60b7eed01bc4c1e60b7f4ba7e
Пестриков Михаил Леонидович <PestrikovML@dom201.local>
2024-05-06 09:20:50 +0700

    full 10 laba

--- laba10/src/App.js
+++ laba10/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import PostAndUserInfo from "./tasks/task1";
-import RegistrationForm from './tasks/task2/task2';
+import FetchForm from './FetchForm';
+
 function App() {
   return (
-    <div className='flex-container'>
-      <div className="flex-item"><PostAndUserInfo /></div>
-      <div className="flex-item"><RegistrationForm/></div>
+    <div>
+      <FetchForm />
     </div>
   );
 }
