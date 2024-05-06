4d2020775e935cde5cc5eb379d9c374448f6b226
Пестриков Михаил Леонидович <PestrikovML@DOM201.LOCAL>
2024-05-06 09:34:31 +0700

    full 10 laba

--- laba10/src/tasks/task3/routers.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-// Define routes and their corresponding controller methods
-router.post('/registration', controller.registration);
-router.post('/login', controller.login);
-router.get('/users', controller.getusers);
-module.exports = router;
